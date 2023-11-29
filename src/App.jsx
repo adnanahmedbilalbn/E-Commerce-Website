431d17099d8990daae1e560dd07f888899e6ae6d
Adnan <adnanahmedbilalbn@gmail.com>
2023-11-30 00:55:50 +0500

    Added Login and Registeration Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import "../src/assets/css/style.css"
 // import MyPageInitialExpanded from './components/Common/initial/InitialPage';
 import Contact from './pages/Contact';
 import Footer from './components/Common/Footer';
+import LoginPage from './components/Login';
+import Register from './components/Register';
+
 
 
 const App = () => {
@@ -17,6 +20,9 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path='/contact' element={<Contact />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<Register />} />
+
           </Routes>
           {/* <MyPageInitialExpanded /> */}
           <Footer />
--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -1,52 +1,46 @@
-// Navbar.js (React Component)
-
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    // const state = useSelector(state => state.handleCart)
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
+            <div className="container">
+                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> React Ecommerce</NavLink>
+                <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav m-auto my-2 text-center">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Home </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/product">Products</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/about">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                        </li>
+                    </ul>
+                    <div className="buttons text-center">
+                        <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Login</NavLink>
+                        <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Register</NavLink>
+                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i>
+                         Cart 
+                         {/* ({state.length})  */}
+                         </NavLink>
+                    </div>
+                </div>
 
-  return (
-    <nav className="navbar">
-      <div className="nav-item">
-        <Link to={"/"}>
-          Home
-        </Link>
-      </div>
-      <div className="nav-item">
-        <div className="dropdown">
-          <div className="dropdown-btn" onClick={toggleDropdown}>
-            <Link to={"products"}>
-                Products
-            </Link>
-          </div>
-          {isDropdownOpen && (
-            <div className="dropdown-content">
-            <div className="dropdown-item">
-              <Link to={"/men"}>
-                Men
-              </Link>
+
             </div>
-            <div className="dropdown-item">
-              <Link to={"/women"}>
-                Women
-              </Link>
-            </div>
-         </div>
-          )}
-        </div>
-      </div>
-      <div className="nav-item">
-          <Link to={"/contact"}>
-                Contact
-          </Link>
-      </div>
-    </nav>
-  );
-};
+        </nav>
+    )
+}
 
-export default Navbar;
+export default Navbar
