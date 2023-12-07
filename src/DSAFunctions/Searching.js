b60aa41086a00495ccfa98d800181eb22efbbb8f
Ramila Kamal <ramila.kl123@gmail.com>
2023-12-07 22:19:29 +0500

    completed linked list

--- src/DSAFunctions/Searching.js
+++ src/DSAFunctions/Searching.js
@@ -1,10 +1,10 @@
 
-const linearSearch = (array, target) => {
+function linearSearch(array, target){
   // console.log(array, target)
     for (let i = 0; i < array.length; i++) {
       if (array[i].title === target) {
         // console.log(array[i].title)
-        return array[i].title;
+        return array[i];
       }
         // console.log(array[i], target)
     }
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,25 +1,15 @@
 import React from "react";
 import { useSelector } from 'react-redux';
-import men from "../api/Men";
-import women from "../api/Women";
-import sports from "../api/Sports";
-import electronics from "../api/Electronics";
 import ProductCard from "../components/Card/ProductCard";
 
 const SearchPage = () => {
 
   const searchItem = useSelector((state) => state?.searchItem);
-  const allProducts = [...men, ...women, ...sports, ...electronics];
-
-  const foundProduct = allProducts.find(product =>
-    product.title && typeof searchItem === 'string' &&
-    product.title.toLowerCase().includes(searchItem.toLowerCase())
-  );
 
   return (
     <div>
-      {foundProduct ? (
-        <ProductCard product={foundProduct} />
+      {searchItem? (
+        <ProductCard product={searchItem} />
       ) : (
         <p>Nothing Found</p>
       )}
