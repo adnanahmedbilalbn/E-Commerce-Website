6632c4a77228703eec062ec933d71adeeaa8ff85
Ramila Kamal <ramila.kl123@gmail.com>
2023-12-25 14:42:10 +0500

    updated version1

--- src/DSAFunctions/Searching.js
+++ src/DSAFunctions/Searching.js
@@ -1,16 +1,11 @@
 
 function linearSearch(array, target){
-  // console.log(array, target)
     for (let i = 0; i < array.length; i++) {
       if (array[i].title === target) {
-        // console.log(array[i].title)
         return array[i];
       }
-        // console.log(array[i], target)
     }
-    // return -1;
     return -1;
   };
   
 export default linearSearch
-  
